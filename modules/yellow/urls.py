1f8feb2afc65aeaadef6f6e6777bacd5cf7df790
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2026-04-30 21:47:48 +0700

    dashboard, login, register routing on the navbar

--- modules/yellow/urls.py
+++ modules/yellow/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from modules.yellow import views
 
-app_name = 'green'
+app_name = 'yellow'
 
 urlpatterns = [
-    
+    path('page/', views.page_view, name='page'),
+    path('form/', views.form_view, name='form'),
 ]
--- modules/yellow/views.py
+++ modules/yellow/views.py
@@ -6,4 +6,26 @@ from modules.green.models import (
 
 # Create your views here.
 
+def page_view(request):
+    members = Member.objects.all()
+    staf = Staf.objects.all()
+    maskapai = Maskapai.objects.all()
+    bandara = Bandara.objects.all()
+    claim_missing_miles = ClaimMissingMiles.objects.all()
+    pengguna = Pengguna.objects.all()
 
+    context = {
+        'members': members,
+        'staf': staf,
+        'maskapai': maskapai,
+        'bandara': bandara,
+        'claim_missing_miles': claim_missing_miles,
+        'pengguna': pengguna,
+    }
+    return render(request, 'yellow/page.html', context)
+
+def form_view(request):
+    if request.method == 'POST':
+        pass
+
+    return render(request, 'yellow/form.html')

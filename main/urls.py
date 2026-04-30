ea58837638a18c5eba4c1ec2487c0ce6f1c31323
Whosmomo <pradiptaadinugroho@gmail.com>
2026-04-30 21:26:46 +0700

    added dashboard template

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import homepage, login, register
+from main.views import homepage, login, register, dashboard_member, dashboard_staff
 
 app_name = 'main'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', homepage, name='home'),
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('dashboard/', dashboard_member, name='dashboard_member'),
+    path('staff/dashboard/', dashboard_staff, name='dashboard_staff'),
 ]
--- main/views.py
+++ main/views.py
@@ -10,3 +10,56 @@ def login(request):
 def register(request):
     # test
     return render(request, 'register.html')
+
+def dashboard_member(request):
+    user_data = {
+        "name": "Mr. John William Doe",
+        "email": "john@example.com",
+        "phone": "+62 81234567890",
+        "nationality": "Indonesia",
+        "birthdate": "1990-05-15",
+        "joined": "2024-01-15"
+    }
+
+    member_data = {
+        "member_id": "M0001",
+        "tier": "Gold",
+        "total_miles": 45000,
+        "award_miles": 32000,
+        "transactions": [
+            {"type": "Transfer", "date": "2025-01-15", "miles": "-5000"},
+            {"type": "Redeem", "date": "2025-01-20", "miles": "-3000"},
+            {"type": "Package", "date": "2025-03-01", "miles": "+10000"},
+        ]
+    }
+
+    return render(request, "dashboard_member.html", {
+        "role": "member",
+        "user": user_data,
+        "member": member_data,
+    })
+
+
+def dashboard_staff(request):
+    user_data = {
+        "name": "Mr. Admin Aero",
+        "email": "admin@aeromiles.com",
+        "phone": "+62 811111111",
+        "nationality": "Indonesia",
+        "birthdate": "1988-01-01",
+        "joined": "2023-01-01"
+    }
+
+    staff_data = {
+        "staff_id": "S0001",
+        "airline": "Garuda Indonesia",
+        "pending": 2,
+        "approved": 1,
+        "rejected": 1
+    }
+
+    return render(request, "dashboard_staff.html", {
+        "role": "staff",
+        "user": user_data,
+        "staff": staff_data,
+    })

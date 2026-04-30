97a309be5cc137425e856a30cb0123dbbdaa8267
Whosmomo <pradiptaadinugroho@gmail.com>
2026-04-30 19:44:44 +0700

    added routing and template for redeem

--- aeromiles/settings.py
+++ aeromiles/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'modules.green',
+    'modules.blue',
 ]
 
 MIDDLEWARE = [
--- modules/blue/apps.py
+++ modules/blue/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class BlueConfig(AppConfig):
-    name = 'blue'
+    name = 'modules.blue'

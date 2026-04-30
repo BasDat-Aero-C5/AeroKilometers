f86dbe8b1fcf66a7378c858f12b41a5f8dd2d913
angelojohenry <johenry.angelo@gmail.com>
2026-04-30 18:15:47 +0700

    add templates for mitra

--- modules/red/urls.py
+++ modules/red/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('hadiah/tambah/', views.tambah_hadiah, name='tambah_hadiah'),
     path('hadiah/edit/', views.edit_hadiah, name='edit_hadiah'),
     path('hadiah/hapus/', views.hapus_hadiah, name='hapus_hadiah'),
+    path('mitra/', views.daftar_mitra, name='daftar_mitra'),
+    path('mitra/tambah/', views.tambah_mitra, name='tambah_mitra'),
+    path('mitra/edit/', views.edit_mitra, name='edit_mitra'),
+    path('mitra/hapus/', views.hapus_mitra, name='hapus_mitra'),
 ]
--- modules/red/views.py
+++ modules/red/views.py
@@ -11,3 +11,15 @@ def edit_hadiah(request):
 
 def hapus_hadiah(request):
     return render(request, 'hadiah/hapus_hadiah.html')
+
+def daftar_mitra(request):
+    return render(request, 'mitra/daftar_mitra.html')
+
+def tambah_mitra(request):
+    return render(request, 'mitra/tambah_mitra.html')
+
+def edit_mitra(request):
+    return render(request, 'mitra/edit_mitra.html')
+
+def hapus_mitra(request):
+    return render(request, 'mitra/hapus_mitra.html')
